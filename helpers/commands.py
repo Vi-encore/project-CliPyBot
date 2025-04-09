092aed980f1a0378c2516dd747ea198d1cfe6719
sanyanut <alexander.sorochinskiy@gmail.com>
2025-04-10 00:44:34 +0300

    add commands suggestions which is partly working with existing codebase TODO: wrap all edit functionality for contacts then notes to make it more responsible for user

---
+++ helpers/commands.py
@@ -0,0 +1,23 @@
+all_available_commands = [
+    'add contact',
+    'find contact',
+    'remove contact',
+    'all contacts',
+    'add phone',
+    'change phone',
+    'show phone',
+    'delete phone',
+    'add email',
+    'change email',
+    'show email',
+    'delete email',
+    'add birthday',
+    'show birthday',
+    'update birthday',
+    'all birthdays',
+    'export contacts'
+]
+
+def commands():
+    return all_available_commands
+commands_list = commands()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from fuzzywuzzy import process
 from services import contacts
 from helpers.helpers import parse_input
+from helpers.commands import commands_list
 from services.shared import show_help, close, hello, goodbye, greeting
 from helpers.typing_effect import typing_input, typing_output
 from rich.console import Console
@@ -7,7 +9,65 @@ from helpers.matrix_effect import matrix_drop
 
 # Initialize Console for rich output
 console = Console()
-   
+
+
+def suggest_and_execute_command(cmd: str, args: list, func):
+    if not cmd or cmd.strip() == "":
+        return False
+
+    # Get suggestion using fuzzywuzzy
+    match = process.extractOne(cmd.strip().lower(), commands_list)
+
+    if match and match[1] >= 70:  # Only consider matches with score of 70 or higher
+        suggested_cmd = match[0]
+
+        # Ask user if they want to execute the suggested command
+        console.print(f'Did you mean "[blue]{suggested_cmd}[/]"?', style='yellow italic')
+        response = typing_input(f'Run "{suggested_cmd}" instead? (y/n): ')
+
+        if response.lower() in ('y', 'yes'):
+            # Execute the suggested command
+            func(suggested_cmd, args)
+            return True
+
+    return False
+
+def execute_command(cmd: str, args: list):
+    if cmd == 'add contact':
+        contacts.add()
+    elif cmd == 'find contact':
+        contacts.find(*args)
+    elif cmd == 'remove contact':
+        contacts.remove(*args)
+    elif cmd == "all contacts":
+        contacts.all()
+    elif cmd == "add phone":
+        contacts.add_phone(*args)
+    elif cmd == "change phone":
+        contacts.change_phone(*args)
+    elif cmd == "show phone":
+        contacts.show_phone(*args)
+    elif cmd == "delete phone":
+        contacts.delete_phone(*args)
+    elif cmd == "add email":
+        contacts.add_email(*args)
+    elif cmd == "change email":
+        contacts.change_email(*args)
+    elif cmd == "show email":
+        contacts.show_email(*args)
+    elif cmd == "delete email":
+        contacts.delete_email(*args)
+    elif cmd == "add birthday":
+        contacts.add_birthday(*args)
+    elif cmd == "show birthday":
+        contacts.show_birthday(*args)
+    elif cmd == "update birthday":
+        contacts.update_birthday(*args)
+    elif cmd == "all birthdays":
+        contacts.all_birthdays()
+    elif cmd == 'export contacts':
+        contacts.export_contacts_to_csv()
+
 def main():
     '''
         Main function for the assistant bot
@@ -39,46 +99,19 @@ def main():
             goodbye()
             break
         # Contact commands 
-        elif cmd == 'add contact': #()
-            contacts.add()
-        elif cmd == 'find contact': #(name)
-            contacts.find(*args)
-        elif cmd == 'remove contact': #(name)
-            contacts.remove(*args)
-        elif cmd == "all contacts":  # ()
-            contacts.all()
-        elif cmd == "add phone":  # (name, phone number)
-            contacts.add_phone(*args)
-        elif cmd == "change phone":  # (name, old phone number , new phone number)
-            contacts.change_phone(*args)
-        elif cmd == "show phone":  # (name)
-            contacts.show_phone(*args)
-        elif cmd == "delete phone":  # (name, phone number)
-            contacts.delete_phone(*args)
-        elif cmd == "add email":  # (name, email)
-            contacts.add_email(*args)
-        elif cmd == "change email":  # (name, old email, new email)
-            contacts.change_email(*args)
-        elif cmd == "show email":  # (name)
-            contacts.show_email(*args)
-        elif cmd == "delete email":  # (name, email)
-            contacts.delete_email(*args)
-        elif cmd == "add birthday":  # (name, birthday)
-            contacts.add_birthday(*args)
-        elif cmd == "show birthday":  # (name)
-            contacts.show_birthday(*args)
-        elif cmd == "update birthday":  # (name, new birthday)
-            contacts.update_birthday(*args)
-        elif cmd == "all birthdays":  # (name, days_to_upcoming)
-            contacts.all_birthdays()
-        elif cmd == 'export contacts': #()
-            contacts.export_contacts_to_csv()
+        elif cmd in commands_list:
+            execute_command(cmd, args)
         # Notes commands
         # logic for notes
         else:
             print('')
             console.print('Unknown command ⚠️', style='red bold')
-            console.print('To get info about available commands, please type [blue]"help"[/] ', style='yellow italic')
+            #console.print('To get info about available commands, please type [blue]"help"[/] ', style='yellow italic')
+            print('')
+            if not suggest_and_execute_command(cmd, args, execute_command):
+                # If no suggestion was executed, show the help message
+                console.print('To get info about available commands, please type [blue]"help"[/] ',
+                              style='yellow italic')
             print('')
          
 if __name__ == '__main__':
